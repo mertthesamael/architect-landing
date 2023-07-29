ff5e3114177719a29b96919b39c4ab5759f73df8
Merto <mertenercan@gmail.com>
2023-07-29 23:50:15 +0300

    Metadata Config

--- app/sharedMetadata.ts
+++ app/sharedMetadata.ts
@@ -1,1 +1,1 @@
-export const openGraphImage = { images: ['../assets/logo.png'] }
+export const openGraphImage = { images: ['https://architect-landing.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.a09cca98.png&w=48&q=75'] }
